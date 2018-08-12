3a5d2ae20cbc1eb438fadcf73d077cef72ec27d9
Stephen Hensley <stephenhensley@Stephens-MacBook-Pro.local>
2018-08-12 11:59:12 -0700

    modified output_hole_locations to be mostly complete. I'd still like it to actually round the values because there is miniscule difference in mm in illustrator and actually converted pts from the api.

--- output_hole_locations.js
+++ output_hole_locations.js
@@ -17,9 +17,19 @@ for (var i = 0; i < sel.length; i++) {
 	var center = obj.position;
 	center[0] += obj.width/2.0; 
 	center[1] -= obj.height/2.0; 
+	var width_mm = pts_to_mm(obj.width);
+	var height_mm = pts_to_mm(obj.height);
 	var center_x_mm = pts_to_mm(center[0] - x_offset);
 	var center_y_mm = pts_to_mm(board_height - (y_offset - center[1]));
-	file.write(obj.name+','+round_dec(center_x_mm, 4)+','+round_dec(center_y_mm, 4)+ "\n");
+	var origin_x_mm = pts_to_mm(obj.position[0]);
+	var origin_y_mm = pts_to_mm(obj.position[1]);
+    if (obj.name.indexOf("pcb") != -1) {
+        // Non circular component. Don't use center of dimension.
+	    file.write(obj.name+','+round_dec(origin_x_mm, 4)+','+round_dec(origin_y_mm, 4)+','+round_dec(width_mm, 4) + ',' + round_dec(height_mm, 4) + "\n");
+    } else {
+        // Circular positions.
+	    file.write(obj.name+','+round_dec(center_x_mm, 4)+','+round_dec(center_y_mm, 4)+','+round_dec(width_mm, 4) + ',' + round_dec(height_mm, 4) + "\n");
+	}
 }
 
 file.close();
